197519a4cdf4771ee279b53a03525a8ed1d19e53
LauraSofiaMuñoz <muozmontoyal@gmail.com>
2017-10-16 21:23:06 -0500

    Correcciones

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,11 +4,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user).permit(:nombre, :genero, :estilo, :email, :password, :password_confirmation)
-
   end
 
   def account_update_params
     params.require(:user).permit(:nombre, :genero, :estilo, :email, :password, :password_confirmation, :current_password)
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  devise_for :user, :controllers => { :registrations => "registrations" }
   root to: 'view#index'
   get 'view/index'
   post 'view/index'
